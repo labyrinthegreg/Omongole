0cc430c0839e70b7df02ee53eddea5c4cd1337bf
johannaT19 <72390893+johannaT19@users.noreply.github.com>
2023-03-31 10:12:04 +0200

    fix tags home + UI

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,48 +3,66 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function Home() {
 
-  const [value, setValue] = useState("");
+    const [value, setValue] = useState("");
+    const [tags, setTags] = useState<string[]>([]);
 
-  function handleKeyPress(event: React.KeyboardEvent<HTMLInputElement>) {
-    if (event.key === "Enter") {
-        const inputValue = (event.target as HTMLInputElement).value;
-        setValue(inputValue);
-        // Vous pouvez stocker la valeur dans une variable ou faire autre chose avec cette valeur ici
+    function handleKeyPress(event: React.KeyboardEvent<HTMLInputElement>) {
+        if (event.key === "Enter") {
+            if (value !== "") {
+                setTags(tags => [value])
+                setValue("")
+            } else {
+                setTags([value])
+            }
+        }
     }
-}
 
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="containerContainerTags">
-          <h1>OMONGOLE</h1>
-          <div className="containerTags">
-            <div className="yourTags">
-              <h3>YOUR TAGS</h3>
-              <p>your tags</p>
+
+
+    return (
+        <div className="App">
+            <div className="container_blur">
+                <div className="omongole">
+                    <h1>
+                        OMONGOLE
+                    </h1>
+                </div>
+                <div className="container_tag">
+                    <div className="container_tags">
+                        <p className="title">
+                            YOUR TAG
+                        </p>
+                        <p>
+                            {tags.map(tag => {return(<span className="tags">{tag}</span>)})}
+                        </p>
+                        <button className="button_reset_container" onClick={() => setTags(tags => [])}>
+                            <p className="button_reset"> RESET TAG</p>
+                        </button>
+                    </div>
+                    <div className="separator"></div>
+                    <div className="container_enter_tag">
+                        <p className="title">
+                            ENTER A TAG
+                        </p>
+                        <input type="text" id="myTag" name="myTag" onKeyPress={handleKeyPress} onChange={e => setValue(e.target.value)} value={value}/>
+                    </div>
+                </div>
+                <div className= "container_button">
+                    <button className="button_container">
+                        <p className="button_text">CHAT VIDEO</p>
+                    </button>
+                    <a href="chat_only_page.tsx">
+                        <button className="button_container">
+                            <p className="button_text">CHAT ONLY</p>
+                        </button>
+                    </a>
+                </div>
             </div>
-            <div className="separator"></div>
-            <div className="tags">
-              <h3>ENTER TAG</h3>
-              <form action="post">
-                <input type="text" className="inputTag" onKeyPress={handleKeyPress}/>
-              </form>
-            </div>
-          </div>
-          <div className="containerButtonChat">
-            <div className="chatVideo">
-              <button>Chat video</button>
-            </div>
-            <div className="chat">
-              <button>Chat</button>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Home
